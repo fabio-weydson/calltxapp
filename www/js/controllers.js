c297e851aa70e69cf219fb68883b04d2c46ffd8e
Weydson <weydson@github.com>
2018-09-24 01:02:41 -0300

    upd21

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -70,7 +70,6 @@ angular.module('starter.controllers', [])
     lng: -42.876664
   };
   function initialize(pos) {
-      alert('iinit');
     $scope.markers = [];
 
     $rootScope.departue.LatLng = pos.lat+','+pos.lng;
@@ -86,6 +85,7 @@ angular.module('starter.controllers', [])
       mapTypeControl: false,
       zoomControl: false,
       streetViewControl: false,
+      fullscreenControl: false,
       styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#7c93a3"},{"lightness":"-10"}]},{"featureType":"administrative.country","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"administrative.country","elementType":"geometry.stroke","stylers":[{"color":"#a0a4a5"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"color":"#62838e"}]},{"featureType":"landscape","elementType":"all","stylers":[{"visibility":"off"},{"lightness":"-29"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#dde3e3"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"geometry.stroke","stylers":[{"color":"#3f4a51"},{"weight":"0.30"},{"visibility":"on"},{"lightness":"74"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.attraction","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.government","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.place_of_worship","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.school","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":"-100"},{"visibility":"on"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#bbcacf"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"lightness":"0"},{"color":"#bbcacf"},{"weight":"0.50"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway.controlled_access","elementType":"geometry.stroke","stylers":[{"color":"#a9b4b8"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"invert_lightness":true},{"saturation":"-7"},{"lightness":"3"},{"gamma":"1.80"},{"weight":"0.01"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"transit.station.bus","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#a3c7df"}]}]
     };
     // init map
@@ -114,12 +114,13 @@ angular.module('starter.controllers', [])
 
     var bounds = new google.maps.LatLngBounds();
     var points = [ new google.maps.LatLng(pos) ];
-  
-    // Extend bounds with each point
-    for (var i = 0; i < points.length; i++) {
-      var marker = new google.maps.Marker({position: points[i], map: $scope.map,  icon: 'img/current_pos.png'});
+    if($scope.my_location) {
+        // Extend bounds with each point
+        for (var i = 0; i < points.length; i++) {
+        var marker = new google.maps.Marker({position: points[i], map: $scope.map,  icon: 'img/current_pos.png'});
+        }
+        $scope.markers.push(marker);
     }
-    $scope.markers.push(marker);
     $timeout(function(){
         $scope.showPlaces = true;
     },1000)
@@ -212,18 +213,21 @@ angular.module('starter.controllers', [])
 }
 
   function getUserLocation(){
-    alert('local');
          navigator.geolocation.getCurrentPosition(function(position) {
-             alert(position.coords.latitude)
+             
+             $scope.my_location = true;
           $rootScope.pos = {
              lat: position.coords.latitude,
              lng: position.coords.longitude
            };
            initialize($rootScope.pos);
          },function (error) {
-            alert('code: ' + error.code + '\n' +
-                  'message: ' + error.message + '\n');
-        },{enableHighAccuracy: true, maximumAge:12000, timeout:10000});
+            $scope.my_location = false;
+            $rootScope.pos = {
+                lat: -20.755986,
+                lng: -42.876664
+              };
+        },{enableHighAccuracy: true, maximumAge:0, timeout:5000});
    }
    
   // load map when the ui is loaded
